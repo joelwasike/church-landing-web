2fc04af1cb579d0995a5f4b02104dc51dad16772
joelwasike <joelwasike97@gmail.com>
2026-08-28 12:14:23 +0300

    sdsd

--- src/components/Brand.tsx
+++ src/components/Brand.tsx
@@ -1,11 +1,7 @@
 export function Brand() {
   return (
     <a className="brand" href="#top">
-      <img src="/images/logo-mark.png" alt="Gather" className="brand__mark brand__mark--image" />
-      <span className="brand__text">
-        <span className="brand__name">Gather</span>
-        <span className="brand__tag">Church all-in-one</span>
-      </span>
+      <img src="/images/logo-mark.png" alt="Gather" className="brand__logo" />
     </a>
   );
 }
